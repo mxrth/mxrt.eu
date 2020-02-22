0cdde2ec725781ef11ec90537d8d12bd055505b9
Max Rath <mail@mxrt.de>
2020-02-22 17:48:04 +0100

    simplify db managment

--- base/pgsql/init/nextcloud.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-PGPASSWORD=$DB_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --password<<-EOSQL
-	    CREATE USER nextcloud WITH PASSWORD=$NEXTCLOUD_DB_PASSWORD;
-	    CREATE DATABASE nextcloud;
-	    GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
-EOSQL
