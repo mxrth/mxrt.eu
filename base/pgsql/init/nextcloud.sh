e3aa24293be5789b63e999360b421427a4c029b1
Max Rath <mail@mxrt.de>
2020-02-22 17:33:58 +0100

    change setup

--- base/pgsql/init/nextcloud.sh
+++ base/pgsql/init/nextcloud.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+PGPASSWORD=$DB_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --password<<-EOSQL
 	    CREATE USER nextcloud WITH PASSWORD=$NEXTCLOUD_DB_PASSWORD;
 	    CREATE DATABASE nextcloud;
 	    GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
