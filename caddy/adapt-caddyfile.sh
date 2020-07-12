73088d39f30c306052d14c449ed225fa8031a0fe
Max Rath <mail@mxrt.de>
2020-07-12 13:48:35 +0200

    only update config when no error occured

--- caddy/adapt-caddyfile.sh
+++ caddy/adapt-caddyfile.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-caddy adapt | jq .apps > conf.apps.json
-
-sudo curl http://localhost/config/apps --unix-socket /run/caddy/api.sock -X POST -H "Content-Type: application/json" -d @conf.apps.json
+caddy adapt | jq .apps > conf.apps.json && sudo curl http://localhost/config/apps --unix-socket /run/caddy/api.sock -X POST -H "Content-Type: application/json" -d @conf.apps.json
