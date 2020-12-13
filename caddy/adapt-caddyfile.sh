87a2e37e49265b230745ec7bff7b1f091f734d94
Max Rath <mail@mxrt.de>
2020-12-13 10:54:46 +0100

    cleanup, new design

--- caddy/adapt-caddyfile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-caddy adapt | jq .apps > conf.apps.json && sudo curl http://localhost/config/apps --unix-socket /run/caddy/api.sock -X POST -H "Content-Type: application/json" -d @conf.apps.json
