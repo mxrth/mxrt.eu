fb2f3b21d26cc61de9895e52bfbeea5d25d630a9
Maximilian Rath <maxr@mailbox.org>
2022-03-20 13:08:00 +0100

    symlinks

---
+++ init/070-nftables.sh
@@ -0,0 +1,2 @@
+sudo cp mxrt.de/etc/nftables.conf /etc/nftables.conf
+sudo systemctl enable nftables.service
---
+++ scripts/make-admin-symlinks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -s home/admin/.bashrc /home/admin/.bashrc
+ln -s home/admin/.bash_profile /home/admin/.bash_profile
